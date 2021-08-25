0195b6b33eccacef3aae8645673fb159a161e5e3
tron001 <a964327641@qq.com>
2021-08-25 21:46:14 +0800

    添加注释

--- Core.Web/Core.Web/Startup.cs
+++ Core.Web/Core.Web/Startup.cs
@@ -40,14 +40,16 @@ namespace Core.Web
                 // 默认HSTS值为30天。您可能需要为生产场景更改此设置.(开启HTTPS时启用) 
                 //app.UseHsts();
             }
+            //// 使用Https的协议TLS
             app.UseHttpsRedirection();
+            //// 访问静态文件
             app.UseStaticFiles();
-
+            //// 注册路由
             app.UseRouting();
 
             //// 身份认证 由于在ASP.NET Core 3.0中，app.UseMvc被app.UseRouting和app.UseEndpoints替代，所以app.UseAuthentication和app.UseAuthorization，要放在app.UseRouting、app.UseCors之后，并且在app.UseEndpoints之前
             app.UseAuthorization();
-
+            //// 端点
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
